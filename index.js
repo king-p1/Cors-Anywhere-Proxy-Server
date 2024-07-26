73a8b63a0a290830903d978c55716da067d163b6
Oladele  Paul <98545430+king-p1@users.noreply.github.com>
2024-07-26 15:46:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,10 @@
-import express from 'express';
-import cors from 'cors';
-import { createProxyMiddleware } from 'http-proxy-middleware';
-const app = express();
-
-app.use(cors({
-  origin: '*', // Adjust as needed to restrict allowed origins
-}));
-
-app.use(express.json())
-
-const PORT = process.env.PORT || 4000;
-
-app.use('/api', createProxyMiddleware({
-  target: 'https://api.creditregistry.com/nigeria/AutoCred/Test/v8',
-  changeOrigin: true,
-  pathRewrite: {
-    '^/api': '', // Remove '/api' prefix when forwarding
-  },
-  onProxyReq: (proxyReq, req, res) => {
-    console.log(`Proxying ${req.method} request to ${proxyReq.path}`);
-  },
-  onError: (err, req, res) => {
-    console.error(`Proxy error: ${err.message}`);
-    res.status(500).json({ error: 'Proxy error' });
-  },
-  onProxyRes: (proxyRes, req, res) => {
-    console.log(`Received ${proxyRes.statusCode} response from target`);
-  }
-}));
-
-app.listen(PORT, () => {
-  console.log(`Proxy server running on ${PORT}`);
+import  corsAnywhere from 'cors-anywhere'  
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 8080;
+corsAnywhere.createServer({
+  originWhitelist: [], // Allow all origins
+  requireHeader: ['origin', 'x-requested-with'],
+  removeHeaders: ['cookie', 'cookie2']
+}).listen(port, host, () => {
+  console.log(`CORS Anywhere server running on ${host}:${port}`);
 });
